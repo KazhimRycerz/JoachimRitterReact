2dd3b98d3b093d1a0b285068a72a96954bda1896
Joachim Ritter <95426840+KazhimRycerz@users.noreply.github.com>
2022-10-11 21:53:22 +0200

    page 404

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import {  
-  BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+  HashRouter as Router, Routes, Route} from 'react-router-dom'
 import Home from './components/Home'
 import PersonalData from './components/PersonalData'
 import Motivation from './components/Motivation'
